72db054aad6639205796589a45f5dc10e7a24b3e
Bobak Farzin <bfarzin@gmail.com>
2019-05-22 08:09:32 -0500

    more changes to scripts for runnning scenarios

--- prod/fit_lm.sh
+++ prod/fit_lm.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
 ## build the tokenizer dataset
-
-# python tok_data.py \
-#        --model-path '../data/rnn/' \
-#        --corpus-fname '../data/all_file.txt'\
-#        --out-pkl-name 'tweet_es_lmdata_test.pkl'\
-#        --sp-model '../__all_tweets_es_0521' \
-#        --vocab-size 60000 \
-#        --batch-size 64 \
-#        --verbose 1
-       
-
+python tok_data.py \
+       --model-path '../data/rnn/' \
+       --corpus-fname '../data/all_file.txt'\
+       --out-pkl-name 'tweet_es_lmdata_test.pkl'\
+       --sp-model '../__all_tweets_es_0521' \
+       --vocab-size 60000 \
+       --batch-size 64 \
+       --verbose 1
 ## fit the model
-
 python main.py \
        --model-path '../data/rnn/' \
        --sp-model '../__all_tweets_es_0521' \
@@ -21,4 +17,3 @@ python main.py \
        --gpu-id 0 \
        --flat_loss 1 \
        --qrnn 1
-
