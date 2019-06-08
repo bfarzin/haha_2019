761d3940ba5af4d4be765d6f5d2f9f9361d625b9
Bobak Farzin <bfarzin@gmail.com>
2019-06-08 12:55:28 -0500

    Update on bpe resutls

--- prod/mult_seed_regr.sh
+++ prod/mult_seed_regr.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for n in {1..200};do \
+for n in {1..61};do \
     python regr.py \
 	   --model-path '../data/rnn/' \
-	   --sp-model '../all_tweets_es_0607_unigram' \
-	   --load_enc 'twitter_es_enc_QRNN_0608_drop1_unigram_seed0' \
+	   --sp-model '../all_tweets_es_0607' \
+	   --load_enc 'twitter_es_enc_QRNN_0607_drop1_seed0' \
 	   --split-seed 20190313 \
 	   --qrnn 1\
     ;
--- prod/mult_seed_run_fwd.sh
+++ prod/mult_seed_run_fwd.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for n in {1..100};do \
+for n in {1..62};do \
     python clas.py \
 	   --model-path '../data/rnn/' \
-	   --sp-model '../all_tweets_es_0607_unigram' \
-	   --load_enc 'twitter_es_enc_QRNN_0608_drop1_unigram_seed0' \
+	   --sp-model '../all_tweets_es_0607' \
+	   --load_enc 'twitter_es_enc_QRNN_0607_drop1_seed0' \
 	   --split-seed 20190313 \
 	   --flat-loss 1 \
 	   --qrnn 1\
